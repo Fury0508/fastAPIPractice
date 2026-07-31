9992d61cdbb1db1f0fc5f95997d68ecef3f72829
Dhananjay Gupta <109964042+Fury0508@users.noreply.github.com>
2026-07-31 18:48:59 +0100

    get to know about the swagger

--- 01-fastapi-foundation.py
+++ 01-fastapi-foundation.py
@@ -1,6 +1,9 @@
+from traceback import StackSummary
+from typing_extensions import deprecated
 from fastapi import FastAPI
 from fastapi import Request
 import uvicorn
+import asyncio
 
 app = FastAPI(
     title = "Swiggy Order Service",
@@ -29,4 +32,74 @@ def about():
         "team":"backend platform",
         "region":"ap-south-1",
         "version":"1.2.2"
+    }
+
+@app.get("/orders")
+def list_orders():
+    """List recent orders """
+    return {
+        "orders":[
+            {"id":1,"item": "Butter Chicken", "status":"delivered"},
+            {"id":2,"item": "Masala Dosa", "status":"preparing"},
+            {"id":3,"item": "Paneer Tikka", "status":"delivered"},
+
+        ]
+    }
+
+@app.get("/orders/status")
+def order_status():
+    """Get oder Status"""
+    return {
+        "total_today":2_340_23,
+        "top_city": "Delhi"
+    }
+
+
+@app.get("/debug/request-info")
+async def request_info(request: Request):
+    """Inspect the raw request object"""
+    return {
+        "method":request.method,
+        "url": str(request.url),
+        "headers": dict(request.headers),
+        "path_params": request.path_params,
+        "query_params": dict(request.query_params),
+    }
+
+
+@app.get(
+    "/orders/active",
+    summary = "Get Active Orders",
+    description = (
+        "Return all orders that are currently beign prepared"
+        "or are out for delivery"
+    ),
+    tags = ['orders'],
+    response_description = "List of active order objects",
+    deprecated = False
+)
+def get_active_order():
+    """This docstring also appears in docs"""
+    return {
+        "active_orders": [
+            {"id": 1, "item": "Masala Dosa","status":"out for delivery"}
+        ]
+    }
+
+@app.get("/restaurants",tags = ['Restaurants'])
+def list_restro():
+    """another docs string for another endpoints"""
+    return {
+        "restaurants":[
+            {"test":"test"}
+        ]
+    }
+
+@app.get("/restaurants/delhi",tags = ['Restaurants',"delhi"])
+def list_restro_delhi():
+    """another docs string for another endpoints"""
+    return {
+        "restaurants":[
+            {"test":"test"}
+        ]
     }
